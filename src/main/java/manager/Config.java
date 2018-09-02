839a7466fa3315da1757a426e1240a18b9738558
privetinternet <37140230+privetinternet@users.noreply.github.com>
2018-09-02 19:58:26 +0300

    Update Config.java

--- src/main/java/manager/Config.java
+++ src/main/java/manager/Config.java
@@ -16,6 +16,7 @@ public class Config {
     private ResourceBundle resource;
     private static final String BUNDLE_NAME = "config";
     public static final String DRIVER = "DRIVER";
+    public static final String ADMINKEY = "ADMINKEY";
     public static final String URL = "URL";
     public static final String MAIN = "MAIN";
     public static final String ERROR = "ERROR";
